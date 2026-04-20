712a38851a4377ec8ff590ab41e6a524eeea16bb
eslamredaegbc-netizen <eslamreda.egbc@gmail.com>
2026-04-20 10:05:52 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          50: "#fffaf0",
+          100: "#f7efd6",
+          200: "#ecdca9",
+          300: "#d9be73",
+          400: "#c59e3b",
+          500: "#b28722",
+          600: "#8b6816",
+          700: "#664c10",
+          800: "#45340b",
+          900: "#261d06"
+        },
+        ink: "#15120b"
+      },
+      boxShadow: {
+        judicial: "0 18px 50px rgba(38, 29, 6, 0.08)"
+      }
+    }
+  },
+  plugins: []
+};
